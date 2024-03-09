3e54adba6c59d9ef2b1ccc67c0fa37f528ed427e
akash <akash300383@gmail.com>
2024-03-09 18:53:44 +0530

    done

--- Phase 1/banking-rest-api-develop-using-python-django/banksystem/api/urls.py
+++ Phase 1/banking-rest-api-develop-using-python-django/banksystem/api/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url 
+from django.urls import include, re_path
 
 from .views import (
     BranchesAPIView,
@@ -17,17 +17,17 @@ from .views import (
 )
 
 urlpatterns = [
-    url(r'^branches/', BranchesAPIView.as_view(),name='branches'),
-    url(r'^branch/(?P<pk>[0-9]+)/', BranchDetailAPIView.as_view(),name='branch-detail'),
-    url(r'^banks', BanksAPIView.as_view(), name='banks'),
-    url(r'^bank/(?P<pk>[0-9]+)/', BankDetailAPIView.as_view(), name='bank-detail'),
-    url(r'^create_account/', CreateAccountAPIView.as_view(), name='create-account'),
-    url(r'^accounts/', AccountListAPIView.as_view(), name='accounts'),
-    url(r'^deposits/', DepositAPIView.as_view(), name='amount deposit'),
-    url(r'^withdrawals/', WithdrawAPIView.as_view(), name='amount withdraw'),
-    url(r'^account/(?P<pk>[0-9]+)/', AccountDetailAPIView.as_view(), name='amount withdraw'),
-    url(r'^signin_account/', ClientSignInAPIView.as_view(), name='signin_account'),
-    url(r'^transfer_amount/', TransferAPIView.as_view(), name='transfer amount'),
-    url(r'^loan/', LoansAPIView.as_view(), name='loan details'),
-    url(r'^customer_request/', CustomerRequestAPIView.as_view(), name='customer request service'), 
+    re_path(r'^branches/', BranchesAPIView.as_view(),name='branches'),
+    re_path(r'^branch/(?P<pk>[0-9]+)/', BranchDetailAPIView.as_view(),name='branch-detail'),
+    re_path(r'^banks', BanksAPIView.as_view(), name='banks'),
+    re_path(r'^bank/(?P<pk>[0-9]+)/', BankDetailAPIView.as_view(), name='bank-detail'),
+    re_path(r'^create_account/', CreateAccountAPIView.as_view(), name='create-account'),
+    re_path(r'^accounts/', AccountListAPIView.as_view(), name='accounts'),
+    re_path(r'^deposits/', DepositAPIView.as_view(), name='amount deposit'),
+    re_path(r'^withdrawals/', WithdrawAPIView.as_view(), name='amount withdraw'),
+    re_path(r'^account/(?P<pk>[0-9]+)/', AccountDetailAPIView.as_view(), name='amount withdraw'),
+    re_path(r'^signin_account/', ClientSignInAPIView.as_view(), name='signin_account'),
+    re_path(r'^transfer_amount/', TransferAPIView.as_view(), name='transfer amount'),
+    re_path(r'^loan/', LoansAPIView.as_view(), name='loan details'),
+    re_path(r'^customer_request/', CustomerRequestAPIView.as_view(), name='customer request service'), 
 ]
