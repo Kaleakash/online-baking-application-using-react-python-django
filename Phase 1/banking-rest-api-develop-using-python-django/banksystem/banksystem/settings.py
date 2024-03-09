02ccf0344819f0025b92634f06d7e28deefa7667
akash <akash300383@gmail.com>
2024-03-09 17:45:26 +0530

    done

--- Phase 1/banking-rest-api-develop-using-python-django/banksystem/banksystem/settings.py
+++ Phase 1/banking-rest-api-develop-using-python-django/banksystem/banksystem/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'banksystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'bank_info_db',
+        'NAME': 'bankdb',
         'HOST': '127.0.0.1',
         'PORT': 27017,
         }
